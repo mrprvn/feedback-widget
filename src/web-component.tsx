ab6b173addd3fa76e16493f17b1a6b2a3faeac6b
Praveen Singh <pvnsingh05@gmail.com>
2025-05-08 22:48:34 +0530

    update index.css

--- src/web-component.tsx
+++ src/web-component.tsx
@@ -5,29 +5,21 @@ import tailwindCSS from "./index.css?inline"; // Tailwind compiled CSS as string
 class FeedbackWebComponent extends HTMLElement {
   projectId: number;
   websiteName: string;
-  shadow: ShadowRoot;
-
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: "open" });
-
+    this.attachShadow({ mode: "open" });
     this.projectId = Number(this.getAttribute("projectId"));
     this.websiteName = this.getAttribute("websiteName") || "";
   }
 
   connectedCallback() {
-    // Create <style> with Tailwind styles
+    const root = document.createElement("div");
     const style = document.createElement("style");
     style.textContent = tailwindCSS;
 
-    // Create div for mounting React app
-    const root = document.createElement("div");
-    root.setAttribute("id", "feedback-root");
+    this.shadowRoot!.appendChild(style);
+    this.shadowRoot!.appendChild(root);
 
-    this.shadow.appendChild(style);
-    this.shadow.appendChild(root);
-
-    // Render React component inside Shadow DOM
     ReactDOM.createRoot(root).render(
       <FeedbackWidget
         projectId={this.projectId}
